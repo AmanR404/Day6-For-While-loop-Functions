4f59abd677ec4b74a5881d6c14d05704c1da2235
Aman <raiaman21@gmail.com>
2022-06-17 11:20:08 +0530

    For Loop

---
+++ Prob2.1 - Factorial with For loop.sh
@@ -0,0 +1,10 @@
+# Get a Factorial of any number
+
+read -p "Enter a Number : " Number
+
+fact=1
+
+for((i=2; i<=Number; i++)){
+result=$((fact=$((fact*i))))
+}
+echo "Factorial of $Number is ---> $result"
